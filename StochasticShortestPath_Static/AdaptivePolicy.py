8a3b1ba7f574eebdd098c28e601ada80cf21e22b
Dennis Janka <dennisjanka@gmail.com>
2023-11-16 07:43:16 +0100

    Updated Adaptive shortest path

--- StochasticShortestPath_Static/AdaptivePolicy.py
+++ StochasticShortestPath_Static/AdaptivePolicy.py
@@ -1,8 +1,7 @@
-import numpy as np
+from collections import namedtuple
 
-from collections import (namedtuple, defaultdict)
 
-class Policy():
+class Policy:
     """
     Base class for Static Stochastic Shortest Path Model policy
     """
@@ -15,22 +14,17 @@ class Policy():
         """
         self.model = model
         self.policy_names = policy_names
-        self.Policy = namedtuple('Policy', policy_names)
+        self.Policy = namedtuple("Policy", policy_names)
 
     def build_policy(self, info):
         # this function builds the policies depending on the parameters provided
         return self.Policy(*[info[k] for k in self.policy_names])
 
-
-    def make_decision(self,M):
-        
-    
+    def make_decision(self, M):
         i = M.state.CurrentNode
 
-        costs = {j:M.state.CurrentNodeLinksCost[j] + M.V_t[j] for j in M.g.edges[i]}
+        costs = {j: M.state.CurrentNodeLinksCost[j] + M.V_t[j] for j in M.g.edges[i]}
 
         optimal_decision = min(costs, key=costs.get)
-        
+
         return optimal_decision, costs[optimal_decision]
-    
-    
