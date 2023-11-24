fa3800aba4b7f7da38aae60c2edd5a847e50564c
Dennis Janka <dennisjanka@gmail.com>
2023-11-24 22:12:19 +0100

    Dynamic SSP formatted

--- StochasticShortestPath_Dynamic/Driver.py
+++ StochasticShortestPath_Dynamic/Driver.py
@@ -21,92 +21,97 @@ from GraphGenerator import GraphGenerator
 from Policy import LookaheadPolicy
 
 
-
-
 if __name__ == "__main__":
-	
-	file = 'Parameters.xlsx'
-	seed = 189654913
-	METRIC = "PERCENTILE"
+    file = "Parameters.xlsx"
+    seed = 189654913
+    METRIC = "PERCENTILE"
 
+    # Reading the algorithm pars
+    parDf = pd.read_excel(file, sheet_name="Parameters")
+    parDict = parDf.set_index("Index").T.to_dict("list")
+    params = {key: v for key, value in parDict.items() for v in value}
+    params["seed"] = seed
+    theta_list = params["theta_cost_set"].split()
 
-	#Reading the algorithm pars
-	parDf = pd.read_excel(file, sheet_name = 'Parameters')
-	parDict=parDf.set_index('Index').T.to_dict('list')
-	params = {key:v for key, value in parDict.items() for v in value}
-	params['seed'] = seed
-	theta_list = params['theta_cost_set'].split()
+    print("Parameters ", params)
 
-	print("Parameters ",params)
+    # Initializing the network
+    G = GraphGenerator(params)
+    if params["networkType"] == "Steps":
+        nTries = G.createNetworkSteps()
+    else:
+        nTries = G.createNetworkChance()
 
-	#Initializing the network
-	G = GraphGenerator(params)
-	if params['networkType'] == 'Steps':
-		nTries = G.createNetworkSteps()
-	else:
-		nTries = G.createNetworkChance()
+    print(
+        "Created network in {} tries. From origin {} to destination {}. Number of steps is {} and the average cost is {:.2f}. The deadline to define lateness will be {:.2f}".format(
+            nTries,
+            G.start_node,
+            G.end_node,
+            G.steps,
+            G.get_avg_cost_paths(),
+            G.get_deadline(),
+        )
+    )
+    # input("Press Enter to continue...")
 
-	print("Created network in {} tries. From origin {} to destination {}. Number of steps is {} and the average cost is {:.2f}. The deadline to define lateness will be {:.2f}".format(nTries,G.start_node,G.end_node,G.steps,G.get_avg_cost_paths(),G.get_deadline()))
-	#input("Press Enter to continue...")
-	
+    # Initializing the model
+    state_names = ["node"]
+    init_state = {"node": G.start_node}
+    decision_names = ["nextNode"]
 
+    M = StaticModel(state_names, decision_names, init_state, params, G)
 
-	# Initializing the model
-	state_names = ['node']
-	init_state = {'node': G.start_node}
-	decision_names = ['nextNode']
+    # Initialing the lists that will hold the results
+    x = []
+    avgCostList = []
+    avgPenaltyList = []
+    avgStepsList = []
 
-	M = StaticModel(state_names, decision_names, init_state, params, G)
+    # Iterating over theta
+    for theta in theta_list:
+        theta = float(theta)
+        M.start_new_theta(theta)
+        x.append(theta)
 
-	
-	# Initialing the lists that will hold the results
-	x = []
-	avgCostList = []
-	avgPenaltyList = []
-	avgStepsList = []
-	
-	# Iterating over theta
-	for theta in theta_list:
-		
-		theta = float(theta)
-		M.start_new_theta(theta)
-		x.append(theta)
-		
-		cost, penalty, steps = M.runTrials(params['nIterations'],G.get_deadline())
+        cost, penalty, steps = M.runTrials(params["nIterations"], G.get_deadline())
 
-		avgCostList.append(cost)
-		avgPenaltyList.append(penalty)
-		avgStepsList.append(steps)
+        avgCostList.append(cost)
+        avgPenaltyList.append(penalty)
+        avgStepsList.append(steps)
 
-		print("Avg total cost with parameter {0} is {1:.3f}. Probability of being late is {2:.2f} and avg number of steps is {3:.2f}\n ".format(theta, cost, penalty,steps))
-	
-	
+        print(
+            "Avg total cost with parameter {0} is {1:.3f}. Probability of being late is {2:.2f} and avg number of steps is {3:.2f}\n ".format(
+                theta, cost, penalty, steps
+            )
+        )
 
-	print("ThetaCost ",x)
-	print("AvgCost ",avgCostList)
-	print("ProbLateness ",avgPenaltyList)
-	print("AvgSteps ",avgStepsList)
+    print("ThetaCost ", x)
+    print("AvgCost ", avgCostList)
+    print("ProbLateness ", avgPenaltyList)
+    print("AvgSteps ", avgStepsList)
 
-	#Ploting the results
-	fig1, axsubs = plt.subplots(1,2)
-	fig1.suptitle('Comparison of theta^cost -  origin {}, destination {}, dist {} - deadline {} and number of iterations {}'.format(M.G.start_node,M.G.end_node,M.G.steps,G.get_deadline(),params['nIterations']) )
-  
+    # Ploting the results
+    fig1, axsubs = plt.subplots(1, 2)
+    fig1.suptitle(
+        "Comparison of theta^cost -  origin {}, destination {}, dist {} - deadline {} and number of iterations {}".format(
+            M.G.start_node,
+            M.G.end_node,
+            M.G.steps,
+            G.get_deadline(),
+            params["nIterations"],
+        )
+    )
 
-	axsubs[0].plot(x, avgCostList)
-	axsubs[0].set_title('Average Cost')
-	axsubs[0].set_xlabel('Percentile')
-	axsubs[0].set_ylabel('$')
+    axsubs[0].plot(x, avgCostList)
+    axsubs[0].set_title("Average Cost")
+    axsubs[0].set_xlabel("Percentile")
+    axsubs[0].set_ylabel("$")
 
-	axsubs[1].plot(x, avgPenaltyList)
-	axsubs[1].set_title('Probability of being late (Risk) ')
-	axsubs[1].set_xlabel('Percentile')
-	axsubs[1].set_ylabel('%')
+    axsubs[1].plot(x, avgPenaltyList)
+    axsubs[1].set_title("Probability of being late (Risk) ")
+    axsubs[1].set_xlabel("Percentile")
+    axsubs[1].set_ylabel("%")
 
-	
-	plt.show()
+    plt.show()
 
-
-
-	
-
-	pass
+    pass
--- StochasticShortestPath_Dynamic/Model.py
+++ StochasticShortestPath_Dynamic/Model.py
@@ -9,7 +9,7 @@ this code. Run the DynamicModel_main instead.
 Author: Andrei Graur 
 
 """
-from collections import (namedtuple, defaultdict)
+from collections import namedtuple, defaultdict
 
 import math
 import numpy as np
@@ -19,158 +19,153 @@ import xlrd
 from Policy import LookaheadPolicy
 
 
-class StaticModel():
-	"""
-	Base class for the static model
-	"""
+class StaticModel:
+    """
+    Base class for the static model
+    """
 
-	def __init__(self, state_names, x_names, s_0, params, G):
-		"""
-		Initializes the model
+    def __init__(self, state_names, x_names, s_0, params, G):
+        """
+        Initializes the model
 
-		:param state_names: list(str) - state variable dimension names
-		:param x_names: list(str) - decision variable dimension names
-		:param s_0: dict - contains the inital state information
-		:param s_0[meanCosts]: dict- meanCosts[k][l] is the mean of the cost on the link k-l 
-		:param s_0[spreads]: dict - spreads[k][l] represents the spread of the distribution of  
-		cost on link k-l 
-		:param Horizon: int - the horizon over which we are looking ahead
-		:param vertexCount - the number of nodes in our network 
-		:param seed: int - seed for random number generator
-		"""
+        :param state_names: list(str) - state variable dimension names
+        :param x_names: list(str) - decision variable dimension names
+        :param s_0: dict - contains the inital state information
+        :param s_0[meanCosts]: dict- meanCosts[k][l] is the mean of the cost on the link k-l
+        :param s_0[spreads]: dict - spreads[k][l] represents the spread of the distribution of
+        cost on link k-l
+        :param Horizon: int - the horizon over which we are looking ahead
+        :param vertexCount - the number of nodes in our network
+        :param seed: int - seed for random number generator
+        """
 
-		self.init_args = params
-		
-		self.init_state = s_0
-		self.state_names = state_names
-		self.State = namedtuple('State', state_names)
-		self.state = self.build_state(self.init_state)
+        self.init_args = params
 
-		self.x_names = x_names
-		self.Decision = namedtuple('Decision', x_names)
-		
-		self.G = G
+        self.init_state = s_0
+        self.state_names = state_names
+        self.State = namedtuple("State", state_names)
+        self.state = self.build_state(self.init_state)
 
-		self.theta = 0.5
-		self.n = 0
-		self.time = 1
-		self.obs = 1
-		self.estimated_costs = defaultdict(dict)
-		self.prng = np.random.RandomState(params['seed'])
-		
-		
-	def start_new_theta(self,theta):
-		self.theta = theta
-		self.estimated_costs = defaultdict(dict)
-		self.n = 0
-		self.obs = 1
-		self.prng = np.random.RandomState(self.init_args['seed'])
-		print("*****************Reseting model for theta {:.2f}".format(self.theta))
+        self.x_names = x_names
+        self.Decision = namedtuple("Decision", x_names)
 
-	def update_estimated_costs(self):
-		for k in range(self.G.vertexCount):
-			for l in self.G.neighbors[k]:
-				m_hat = self.sample_from_uniform(k,l)	
-				alpha = self.get_step_size()
-				if alpha < 1:
-					self.estimated_costs[k][l] = (1-alpha)* self.estimated_costs[k][l] + alpha * m_hat
-				else:
-					self.estimated_costs[k][l] = m_hat
-		
-		self.estimated_costs[self.G.end_node][self.G.end_node] = 0
-		
+        self.G = G
 
-	def sample_from_uniform(self,fromNode,toNode):
-		spread = self.G.spreads[fromNode][toNode]
-		deviation = self.prng.uniform(- spread, spread) * self.G.meanCosts[fromNode][toNode]
-		m_hat = self.G.meanCosts[fromNode][toNode] + deviation	
-		return m_hat
+        self.theta = 0.5
+        self.n = 0
+        self.time = 1
+        self.obs = 1
+        self.estimated_costs = defaultdict(dict)
+        self.prng = np.random.RandomState(params["seed"])
 
+    def start_new_theta(self, theta):
+        self.theta = theta
+        self.estimated_costs = defaultdict(dict)
+        self.n = 0
+        self.obs = 1
+        self.prng = np.random.RandomState(self.init_args["seed"])
+        print("*****************Reseting model for theta {:.2f}".format(self.theta))
 
-	def get_step_size(self):
-		#alpha = 1/self.n
-		#alpha = 1./self.time
-		alpha = 1./self.obs
-		return 	alpha	
+    def update_estimated_costs(self):
+        for k in range(self.G.vertexCount):
+            for l in self.G.neighbors[k]:
+                m_hat = self.sample_from_uniform(k, l)
+                alpha = self.get_step_size()
+                if alpha < 1:
+                    self.estimated_costs[k][l] = (1 - alpha) * self.estimated_costs[k][
+                        l
+                    ] + alpha * m_hat
+                else:
+                    self.estimated_costs[k][l] = m_hat
 
-	def build_state(self, info):
-		return self.State(*[info[k] for k in self.state_names])
+        self.estimated_costs[self.G.end_node][self.G.end_node] = 0
 
-	def build_decision(self, info):
-		return self.Decision(*[info[k] for k in self.x_names])
+    def sample_from_uniform(self, fromNode, toNode):
+        spread = self.G.spreads[fromNode][toNode]
+        deviation = (
+            self.prng.uniform(-spread, spread) * self.G.meanCosts[fromNode][toNode]
+        )
+        m_hat = self.G.meanCosts[fromNode][toNode] + deviation
+        return m_hat
 
-	# exog_info_fn: function - returns the real experienced cost of traversing a link 
-	# from 'fromNode' to 'toNode' 
-	def exog_info_fn(self, fromNode, toNode):
-		cost_hat = self.sample_from_uniform(fromNode,toNode)
-		return cost_hat
+    def get_step_size(self):
+        # alpha = 1/self.n
+        # alpha = 1./self.time
+        alpha = 1.0 / self.obs
+        return alpha
 
-	# transition_fn: function - updates the state within the model and returns new state
-	def transition_fn(self, decision):
-		self.state = self.build_state({'node':decision})
-		self.time += 1
-		self.obs += 1
+    def build_state(self, info):
+        return self.State(*[info[k] for k in self.state_names])
 
-	# :param objective_fn: function - returns the cost we would experience by taking 'decision'
-	# as our next node from the current state 'state'
-	def objective_fn(self, decision):
-		cost = self.exog_info_fn(self.state.node, decision)
-		return cost 
+    def build_decision(self, info):
+        return self.Decision(*[info[k] for k in self.x_names])
 
+    # exog_info_fn: function - returns the real experienced cost of traversing a link
+    # from 'fromNode' to 'toNode'
+    def exog_info_fn(self, fromNode, toNode):
+        cost_hat = self.sample_from_uniform(fromNode, toNode)
+        return cost_hat
 
-	'''
+    # transition_fn: function - updates the state within the model and returns new state
+    def transition_fn(self, decision):
+        self.state = self.build_state({"node": decision})
+        self.time += 1
+        self.obs += 1
+
+    # :param objective_fn: function - returns the cost we would experience by taking 'decision'
+    # as our next node from the current state 'state'
+    def objective_fn(self, decision):
+        cost = self.exog_info_fn(self.state.node, decision)
+        return cost
+
+    """
 	the function for running trials; it simulates solving the problem a bunch of 
 	times (nbTrials times), then takes the squared mean of the costs incurred, 
 	and then returns that mean value
-	'''
-	def runTrials(self,nbTrials,deadline):
-		
-		
-		# variables to store values along iterations 
-		totalPenalty = 0.0
-		totalCost = 0.0
-		totalTime = 0.0
-		
+	"""
 
-		for i in range(nbTrials):
-			
-			self.state = self.build_state(self.init_state)
-			self.time = 1
-			self.n += 1
-			cost=0.0
-			#print("Theta {:.2f} Iteration {}".format(self.theta,self.n))
-			
+    def runTrials(self, nbTrials, deadline):
+        # variables to store values along iterations
+        totalPenalty = 0.0
+        totalCost = 0.0
+        totalTime = 0.0
 
-			#Following a path  - the policy function is a lookahead 
-			while self.state.node != self.G.end_node:
-				self.update_estimated_costs()
-				P = LookaheadPolicy(self) 
-				decision = P.get_decision("PERCENTILE")	
-				#self.build_decision({'nextNode':decision})
-				stepCost = self.objective_fn(decision)
-				cost += stepCost
-				#print("\t Theta {:.2f}, Iteration {}, Time {}, CurrNode {}, Decision {}, Step Cost {:.2f} Cum Cost {:.2f}".format(self.theta,self.n,self.time,self.state.node,decision,stepCost,cost))
-				self.transition_fn(decision)
+        for i in range(nbTrials):
+            self.state = self.build_state(self.init_state)
+            self.time = 1
+            self.n += 1
+            cost = 0.0
+            # print("Theta {:.2f} Iteration {}".format(self.theta,self.n))
 
-			
-			#end of path calculations
-			totalCost += cost
-			if cost > deadline:
-				#latenessSquared = (cost - deadline) ** 2
-				latenessSquared = 1
-				totalPenalty += latenessSquared
-			else:
-				latenessSquared=0
-			totalTime += self.time-1
-			print("End of Theta {:.2f}, Iteration {}. Cost: {:.2f}, Lateness: {:.2f} and number of steps {}".format(self.theta,self.n,cost,math.sqrt(latenessSquared),self.time-1))
+            # Following a path  - the policy function is a lookahead
+            while self.state.node != self.G.end_node:
+                self.update_estimated_costs()
+                P = LookaheadPolicy(self)
+                decision = P.get_decision("PERCENTILE")
+                # self.build_decision({'nextNode':decision})
+                stepCost = self.objective_fn(decision)
+                cost += stepCost
+                # print("\t Theta {:.2f}, Iteration {}, Time {}, CurrNode {}, Decision {}, Step Cost {:.2f} Cum Cost {:.2f}".format(self.theta,self.n,self.time,self.state.node,decision,stepCost,cost))
+                self.transition_fn(decision)
 
-		#end of trials
-		avgCost = totalCost/nbTrials
-		avgPenalty = totalPenalty / nbTrials
-		avgTime = totalTime / nbTrials
+            # end of path calculations
+            totalCost += cost
+            if cost > deadline:
+                # latenessSquared = (cost - deadline) ** 2
+                latenessSquared = 1
+                totalPenalty += latenessSquared
+            else:
+                latenessSquared = 0
+            totalTime += self.time - 1
+            print(
+                "End of Theta {:.2f}, Iteration {}. Cost: {:.2f}, Lateness: {:.2f} and number of steps {}".format(
+                    self.theta, self.n, cost, math.sqrt(latenessSquared), self.time - 1
+                )
+            )
 
+        # end of trials
+        avgCost = totalCost / nbTrials
+        avgPenalty = totalPenalty / nbTrials
+        avgTime = totalTime / nbTrials
 
-		return avgCost,avgPenalty,avgTime
-
-
-
+        return avgCost, avgPenalty, avgTime
